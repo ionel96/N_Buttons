def0a4491beee77b6ed90e2bfe420d82d6546ddc
Ionel Boicu <119396656+ionel96@users.noreply.github.com>
2022-12-08 11:25:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ function createButtons(numOfButtons) {
     let winnerButton = Math.ceil(Math.random() * numOfButtons);
     for (let i = 1; i <= numOfButtons; ++i) {
         let button = document.createElement("button");
-        let nButtons = document.getElementById("n_buttons");
-        button.innerHTML= i;
-        nButtons.appendChild(button);
+        button.innerHTML = i;
+        document.body.appendChild(button);
+        button.id = i;
     }
-}
-function checkButton(clickedButton) {
-    if (Number(clickedButton) == winnerButton) {
-        document.getElementById("output").innerHTML = "Congratulations. You guessed the button!"
-    } else {
-        document.getElementById("output").innerHTML = "Keep trying."
-    }
+    document.querySelectorAll('button').forEach(occurence => {
+        let id = occurence.getAttribute('id');
+        occurence.addEventListener('click', () => {
+            if (Number(id) == winnerButton) {
+                document.getElementById("output").innerHTML = "Congratulations. You guessed the button!"
+            } else {
+                document.getElementById("output").innerHTML = "Keep trying."
+            }
+        });
+    });
 }
